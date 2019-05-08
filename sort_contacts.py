93b5acc073681ddfa36656404cb671862f695af2
Nearlybird <49175582+Nearlybird@users.noreply.github.com>
2019-05-08 14:40:52 -0600

    Update sort_contacts.py

--- sort_contacts.py
+++ sort_contacts.py
@@ -3,19 +3,26 @@ def testEqual(actual, expected):
         print("Pass")
     else:
         print("Fail. Expected:", expected, "but got:", actual)
-   
-def sort_contacts(con_dict):
-    keylist = list(con_dict.keys())
-    keylist.sort()
-    Output = []
-    for key in keylist:
-        Output.append(key)
-        #print(key)
-        Output.append(con_dict[key])
-        #print(con_dict[key])
-    return Output
 
-#sort_contacts({"Horney, Karen": ("1-541-656-3010", "karen@psychoanalysis.com"), "Welles, Orson": ("1-312-720-8888", "orson@notlive.com"), "Freud, Anna": ("1-541-754-3010", "anna@psychoanalysis.com")})
+def sort_contacts(contact_dict):
+    #turn the dictionary into a list (of, hmm, items?)
+    listy = list(contact_dict.items())
+    #create an empty list (that the tuples will go into)
+    output = []
+    #iterate over the items in the list  (ex: for something in something)
+    for item in listy:
+        (name, number, email) = (item[0], item[1][0], item[1][1])
+        tuple1 = (name, number, email)
+        output.append(tuple1)
+    #we assign, as a tuple, the values in the dictionary list
+        #note a tuple is like this `(val1, val2, etc...)`
+        #a tuple is different from a list in that it's values cannot be modified, or that it is `"immutable"`
+        #but, you can access values in a `tuple` the same way you can access a `list`.
+        #ie `tuple[0], tuple[1]`
+    #sort the list
+    output.sort()
+    #return the list
+    return output
 
 testEqual(sort_contacts({"Horney, Karen": ("1-541-656-3010", "karen@psychoanalysis.com"),
        "Welles, Orson": ("1-312-720-8888", "orson@notlive.com"),
